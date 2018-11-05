ceba461c9bff8d4500bc4ec84eca22b6095d0a0f
Devon <devn64@gmail.com>
2018-11-05 23:39:59 +0000

    broken for loop

---
+++ WebApplication1/Pages/Proof.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class ProofModel : PageModel
+    {
+        public string Message { get; set; }
+        public string Loop { get; set; }
+
+        public void OnGet()
+        {
+            Message = "This is a test page to see how the navigation bar handles new pages.";
+        
+            for (; ; )
+            {
+                Loop ="LOOOOOOPS";
+            }
+        }
+    }
+}
+
