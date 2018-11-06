39792adc89056b43fa1e9ccd2b0462d86d13924b
unknown <v-coree@microsoft.com>
2018-11-06 16:14:27 +0000

    I do what I want

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # appServiceLabTwo
-This is the lab for the App Services team at a secret base on the moon.
+This is the lab for the App Services team at a secret base on the moon....
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -25,7 +25,7 @@ namespace WebApplication1
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
-            {
+            { 
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
